3308e0fcf08b3bd002092f8d9f8cd95f2fada9bc
rickycloud <93150146+rickycloud@users.noreply.github.com>
2022-12-10 17:54:34 +0100

    aggiunti film e posti
    manca sistemare grafica film e controllare i posti già occupati

---
+++ Info/Fanin_Riccardo_Esercitazione9_RossiCinema/script.js
@@ -0,0 +1,59 @@
+var seatingChart = document.getElementById('seating-chart');
+var seatSelected = [];
+
+function generateSeats()
+{
+    for (let i = 0; i < 6; i++) {
+        document.getElementById('seating-chart').innerHTML += "<div class='flex mb-4'>";
+        for(let j = 0; j<11; j++)
+        {
+            document.getElementById('seating-chart').innerHTML += "<div id='r" + i + "-c" + j + "' class='seat text-center'></div>";
+        }
+        document.getElementById('seating-chart').innerHTML += "</div>";
+    }
+
+    count = 1
+    Array.from(document.getElementsByClassName('seat')).forEach(element => {
+        element.innerHTML = count++;
+    });
+}
+
+generateSeats();
+
+seatingChart.addEventListener('click', function(event) {
+    if (event.target.classList.contains('seat')) {
+        if(event.target.classList.contains('selected'))
+        {
+            for (let i = 0; i < seatSelected.length; i++) {
+                if(seatSelected[i] == event.target.id)
+                    seatSelected.splice(i, 1);
+            }
+
+            let str = document.getElementById('seat-list').innerHTML;
+            str = str.replace(str.slice(str.length - 4, str.length), "");
+            document.getElementById('seat-list').innerHTML = str;
+
+            event.target.classList.remove('selected');
+        }
+        else
+        {
+            document.getElementById('seat-list').innerHTML += event.target.innerHTML + ', ';
+            seatSelected.push(event.target.id);
+            event.target.classList.toggle('selected');
+        }
+
+    }
+    console.log(seatSelected);
+});
+
+
+
+$('button').click(function() {
+    $.ajax({
+        type: "POST",
+        url: "server/prenotazioni.php",
+        data: { seat: seatSelected }
+    }).done(function( msg ) {
+        alert( "Data Saved: " + msg );
+    });
+});
